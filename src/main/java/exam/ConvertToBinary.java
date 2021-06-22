bee7c1215de95a3c56acff9e0c5fce72d5202381
VishuMore <86244751+VishuMore@users.noreply.github.com>
2021-06-22 13:05:01 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,25 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
+    for( int i=31;i>=0;i--)
+    {
+      int k=n>>i;
+      if((k & 1)>0)
+         System.out.print(" 1 ");
+      else
+        System.out.print(" 0 ");
+      
+    }
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
     return "";
   }
+}
+class Abc
+{
+public static void main(String[] args)
+{
+  ConvertToBinary c = new ConvertToBinary();
+  int n =35;
+  c.toBinary(n);
+}
 }
